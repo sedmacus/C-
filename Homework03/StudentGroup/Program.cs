13c5c15f1a5ac5e2d47be9a0b8c1b3ec1fe3bf8b
Martin Brzovski <martin_brzi@hotmail.com>
2022-03-16 13:26:27 +0100

    Homework

---
+++ Homework03/StudentGroup/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[5] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+
+            string[] studentsG2 = new string[5] { "Pavel", "Vladimir", "Martin", "Vesna", "Nikola" };
+
+            while (true)
+            {
+                Console.WriteLine("Please enter student group 1 or 2:");
+                string input = Console.ReadLine();
+                int.TryParse(input, out int number);
+
+                if (number == 1)
+                {
+                    foreach (string student in studentsG1)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                }
+                else if (number == 2)
+                {
+                    foreach (string student in studentsG2)
+                    {
+                        Console.WriteLine(student);
+                    }
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ Homework03/SumOfEven/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Please insert number " + (i + 1));
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out int number))
+                    {
+                        Console.WriteLine("Please insert number!");
+                        continue;
+                    }
+                    else
+                    {
+                        array[i] = number;
+                        break;
+                    }
+                }
+            }
+            int sum = 0;
+            foreach (int i in array)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("Sum of all even numbers is: " + sum);
+        }
+    }
+}
