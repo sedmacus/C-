5e4991c24e50bc81f750f444aaf299ae3d3a7c5a
Martin Brzovski <martin_brzi@hotmail.com>
2022-03-31 19:49:57 +0200

    Homework

---
+++ HomeworkClass08/HomeworkClass08/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeworkClass08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Please insert number");
+                
+                if(int.TryParse(Console.ReadLine(), out int number))
+                {
+                    numbers.Enqueue(number);
+                }
+                else
+                {
+                    Console.WriteLine("This is not a number!");
+                }
+
+                Console.WriteLine("Do you want to enter another number Y/N");
+                string input = Console.ReadLine();
+
+                if(input.ToLower() != "y")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    continue;
+                }
+            }
+
+            foreach(int i in numbers)
+            {
+                Console.WriteLine($"You entered: {i}");
+            }
+        }
+    }
+}
